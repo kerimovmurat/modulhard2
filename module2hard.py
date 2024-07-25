fff08c9e4a86d8295bfafd1782062d87184ed5a1
kerimovmurat <kerimovmurat@yandex.ru>
2024-07-25 21:08:53 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,7 @@
+n = int(input('Введите число от 3 до 20: '))
+result = []
+for i in range(1, n):
+    for j in range(i + 1, n):
+        if n % (i + j) == 0:
+            result.append(str(i)+ str(j))#+ str(j) , sep=''
+print(*result, sep='')
